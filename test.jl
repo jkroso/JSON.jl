b36875375fddc56c02ed14a350a50c619c106aab
Jake Rosoman <jkroso@gmail.com>
2014-10-07 12:52:23 +1300

    tidy test

--- test.jl
+++ test.jl
@@ -1,11 +1,5 @@
 @require "." JSON
 
-function buffer(data)
-  buf = IOBuffer()
-  writemime(buf, "application/json", data)
-  takebuf_array(buf)
-end
-
 @test JSON(["a"=>1,"b"=>2]) == """{"b":2,"a":1}"""
 @test JSON([1,true,"3"]) == """[1,true,"3"]"""
 @test JSON(["a"=>1]) == """{"a":1}"""
@@ -13,6 +7,6 @@ end
 @test JSON(1.0) == "1.0"
 
 suite("strings") do
-  @test buffer("a") == Uint8['"', 'a', '"']
-  @test buffer("\"") == Uint8['"', '\\', '"', '"']
+  @test [JSON("a")...] == ['"', 'a', '"']
+  @test [JSON("\"")...] == ['"', '\\', '"', '"']
 end
